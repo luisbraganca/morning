65a28023d05a488eee1fbafcfd3f13ca4e3aabf8
Luís Bragança <luisbraganca@protonmail.com>
2018-04-03 17:03:09 +0100

    Added support for old browsers

--- morning/js/main.js
+++ morning/js/main.js
@@ -159,7 +159,7 @@ var main = (function () {
         if (searchEnginesElement.style.maxHeight) {
             searchEnginesElement.style.maxHeight = null;
         } else {
-            searchEnginesElement.style.maxHeight = Math.floor(window.screen.height / 3) + "px";
+            searchEnginesElement.style.maxHeight = "300px";
         }
     };
     var searchEngineHandler = function (engineKey) {
