b188ef583468284ede46e264941fc3511bfb1da5
Luís Bragança <luisbraganca@protonmail.com>
2018-04-09 10:39:56 +0100

    Changed service-worker.js path to experiment

--- morning/js/register-service-worker.js
+++ morning/js/register-service-worker.js
@@ -1,7 +1,7 @@
 (function () {
-    if ("./js/service-worker.js" in navigator) {
+    if ("service-worker.js" in navigator) {
         window.addEventListener('load', function () {
-            navigator.serviceWorker.register("../service-worker.js").then(function (registration) {
+            navigator.serviceWorker.register("./service-worker.js").then(function (registration) {
                 // Registration was successful
                 console.log(registration.scope);
             }, function (err) {
